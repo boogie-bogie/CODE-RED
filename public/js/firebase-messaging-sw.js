d34d12d043afd15e6040d70a27d36b0823de9c46
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-04-24 22:58:54 +0900

    initial CI/CD

--- public/js/firebase-messaging-sw.js
+++ public/js/firebase-messaging-sw.js
@@ -2,13 +2,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
 
 firebase.initializeApp({
-  apiKey: 'FIREBASE_API_KEY',
-  authDomain: 'FIREBASE_AUTH_DOMAIN',
-  projectId: 'FIREBASE_PROJECT_ID',
-  storageBucket: 'FIREBASE_STORAGEBUCKET',
-  messagingSenderId: 'FIREBASE_MESSAGING_SENDER_ID',
-  appId: 'FIREBASE_APP_ID',
-  measurementId: 'FIREBASE_MEASUREMENT_ID',
+  apiKey: 'AIzaSyBWKGEzLjBA-e4PKfp5F9-SyTGgJ1-rGBw',
+  authDomain: 'codered-9bb03.firebaseapp.com',
+  projectId: 'codered-9bb03',
+  storageBucket: 'codered-9bb03.appspot.com',
+  messagingSenderId: '377609042128',
+  appId: '1:377609042128:web:985550c7edd8ee16c686d5',
+  measurementId: 'G-RS03446VPC',
 });
 
 const messaging = firebase.messaging();
