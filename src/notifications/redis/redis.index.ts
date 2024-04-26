66baa7316c4986b1cf42e74ff3d87229eb4df8fd
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-04-27 02:12:17 +0900

    :ambulance: hotfix hotfix-----redis debuging

--- src/notifications/redis/redis.index.ts
+++ src/notifications/redis/redis.index.ts
@@ -22,7 +22,14 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
     this.redisClient = new Redis({
       host: process.env.REDIS_HOST,
       port: parseInt(process.env.REDIS_PORT),
-      password: process.env.REDIS_PASSWORD || undefined,
+      password: process.env.REDIS_PASSWORD,
+      retryStrategy: (times) => {
+        // 3초 후에 재연결 시도, 최대 10번까지 재시도
+        if (times < 10) {
+          return 3000; // 3초 후 재시도
+        }
+        return null; // 더 이상 재시도하지 않음
+      },
     });
   }
 
@@ -41,12 +48,19 @@ export class RedisService implements OnModuleInit, OnModuleDestroy {
     );
 
     this.redisClient.on('connect', () => {
-      console.log('Connected to Redis');
+      console.log('################################### Connected to Redis');
+    });
+
+    this.redisClient.on('error', (err) => {
+      console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 레디스 에러', err);
     });
   }
 
   onModuleDestroy() {
     this.redisClient.disconnect();
+    console.log(
+      '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Redis connection closed.',
+    );
   }
 
   get client(): Redis {
